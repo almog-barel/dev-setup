b64cf14721fabdb17001f9364ca73840183504ed
almog bar-el <almogb@nanolocksec.com>
2022-10-31 17:06:01 +0200

    add git config to .dotfiles

--- link.ps1
+++ link.ps1
@@ -1,4 +1,9 @@
 # making symboing link - New-Item -Path <to> -ItemType SymbolicLink -Value <from>
 
 # link the powershell profile to its intended loacation
-New-Item -Path $profile -ItemType SymbolicLink -Value ./Microsoft.PowerShell_profile.ps1
+del $profile
+New-Item -Path $profile -ItemType SymbolicLink -Value .\Microsoft.PowerShell_profile.ps1
+
+# git config
+del ~\.gitconfig
+New-Item -Path ~\.gitconfig -ItemType SymbolicLink -Value .\.dotfles\.gitconfig
