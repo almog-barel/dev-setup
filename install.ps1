0b0c5482d6c637ef64c2f722deeff99decf5104d
almog bar-el <almogb@nanolocksec.com>
2022-10-31 10:54:20 +0200

    add a script that uses winget to install all needed programs to a new machine

---
+++ install.ps1
@@ -0,0 +1,35 @@
+# first install
+
+# treminal
+winget install -e --id starship
+winget install -e --id JanDeDobbeleer.OhMyPosh
+winget install -e --id Microsoft.WindowsTerminal
+
+# IDEs
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudio.2022.Community
+
+# programin languges
+winget install -e --id GoLang.Go.1.18
+winget install -e --id OpenJS.NodeJS.LTS
+winget install -e --id Python.Python.3.9
+winget install -e --id Microsoft.DotNet.SDK.6
+
+# dev tools
+winget install -e --id Git.Git
+winget install -e --id stedolan.jq
+winget install -e --id GnuWin32.Make
+winget install -e --id dbeaver.dbeaver
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Atlassian.Sourcetree
+winget install -e --id WiresharkFoundation.Wireshark
+
+# other
+winget install -e --id Zoom.Zoom
+winget install -e --id 7zip.7zip
+winget install -e --id PuTTY.PuTTY
+winget install -e --id Google.Chrome
+winget install -e --id Canonical.Ubuntu
+winget install -e --id Grammarly.Grammarly
+winget install -e --id Microsoft.PowerToys
+winget install -e --id SlackTechnologies.Slack
