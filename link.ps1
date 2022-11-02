124bb7fbb160ba8d891d890ab94f403c1bd9feee
almog bar-el <almogb@nanolocksec.com>
2022-11-02 12:17:22 +0200

    no need to delete the file just repleace if exist

--- link.ps1
+++ link.ps1
@@ -1,9 +1,7 @@
 # making symboing link - New-Item -Path <to> -ItemType SymbolicLink -Value <from>
 
 # link the powershell profile to its intended loacation
-del $profile
-New-Item -Path $profile -ItemType SymbolicLink -Value .\Microsoft.PowerShell_profile.ps1
+New-Item -Path $profile -ItemType SymbolicLink -Value .\Microsoft.PowerShell_profile.ps1 -Force
 
 # git config
-del ~\.gitconfig
-New-Item -Path ~\.gitconfig -ItemType SymbolicLink -Value .\.dotfles\.gitconfig
+New-Item -Path ~\.gitconfig -ItemType SymbolicLink -Value .\.dotfles\.gitconfig -Force
