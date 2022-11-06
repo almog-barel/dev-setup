24228ced54f290cba6b8b56ba088e80eff715e49
almog bar-el <almogb@nanolocksec.com>
2022-11-06 10:42:22 +0200

    add vs code extension installation to the install script

--- install.ps1
+++ install.ps1
@@ -15,7 +15,6 @@ npm install -g tldr
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.VisualStudio.2022.Community
 
-
 # dev tools
 winget install -e --id Git.Git
 winget install -e --id stedolan.jq
@@ -34,3 +33,44 @@ winget install -e --id Canonical.Ubuntu
 winget install -e --id Grammarly.Grammarly
 winget install -e --id Microsoft.PowerToys
 winget install -e --id SlackTechnologies.Slack
+
+# vs code extension
+# code --install|uninstall-extension
+code --install-extension 2gua.rainbow-brackets
+code --install-extension aaron-bond.better-comments
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension atlassian.atlascode
+code --install-extension CodeStream.codestream
+code --install-extension cweijan.vscode-postgresql-client2
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension GitHub.remotehub
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension hoovercj.vscode-power-mode
+code --install-extension IronGeek.vscode-env
+code --install-extension janisdd.vscode-edit-csv
+code --install-extension khaeransori.json2csv
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mhutchie.git-graph
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.dotnet-interactive-vscode
+code --install-extension ms-ossdata.vscode-postgresql
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.hexeditor
+code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode.remote-repositories
+code --install-extension phoihos.csv-to-md-table
+code --install-extension rangav.vscode-thunder-client
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension rioukkevin.vscode-git-commit
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension usernamehw.errorlens
+code --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
+code --install-extension VisualStudioExptTeam.vscodeintellicode
